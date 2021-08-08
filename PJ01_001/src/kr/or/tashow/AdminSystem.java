ddf5c10cc4f0cc3cddd1b7034665e7209f277415
Heewon-Seo <seoheewon.dev@gmail.com>
2021-08-08 15:13:17 +0900

    ver1.10 결제 기능, IO 클래스 초기 시스템 시작 시 로드하는 함수명 수정, 테스트용 반납시간 설정 함수 추가

--- PJ01_001/src/kr/or/tashow/AdminSystem.java
+++ PJ01_001/src/kr/or/tashow/AdminSystem.java
@@ -1,6 +1,5 @@
 package kr.or.tashow;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -9,9 +8,12 @@ import static kr.or.tashow.BikeService.bikeList;
 public class AdminSystem {
     Bike bike;
     String id;
+    String pw;
     BikeService bikeService;
+    Menu menu;
     int countSingle;
     int countTwin;
+    HashMap<String, String> adminMap = new HashMap<>();
 
     public AdminSystem() {
         bike = new Bike();
@@ -19,6 +21,8 @@ public class AdminSystem {
         this.countSingle = 0;
         this.countTwin = 0;
         this.id = "";
+        this.pw = "";
+        adminMap.put("admin", "admin!");
     }
 
     HashMap<String, Bike> addBike() {
@@ -29,30 +33,56 @@ public class AdminSystem {
         if (!(input == 1 || input == 2)) {
             System.out.println("다시 입력해주세요.");
         } else {
-        System.out.println("자전거 대수를 입력하세요");
-        int amount = Integer.parseInt(scan.nextLine());
-        if (input == 1) {
-            for (int i = 0 ; i < amount ; i++) {
-                id = String.format("S-%04d", ++countSingle);
-                bikeList.put(id,new Bike(BikeType.Single,1000));
+            System.out.println("자전거 대수를 입력하세요");
+            int amount = Integer.parseInt(scan.nextLine());
+            String bikeId = "";
+            if (input == 1) {
+                for (int i = 0; i < amount; i++) {
+                    bikeId = String.format("S-%04d", ++countSingle);
+                    bikeList.put(bikeId, new Bike(BikeType.Single, 1000));
+                }
+            } else if (input == 2) {
+                for (int i = 0; i < amount; i++) {
+                    bikeId = String.format("T-%04d", ++countTwin);
+                    bikeList.put(bikeId, new Bike(BikeType.Twin, 2000));
+                }
+            } else {
+                System.out.println("잘못 입력");
             }
-        } else if (input == 2) {
-            for (int i = 0 ; i < amount ; i++) {
-                id = String.format("T-%04d",++countTwin);
-                bikeList.put(id,new Bike(BikeType.Twin,2000));
-            }
-        } else {
-            System.out.println("잘못 입력");
-        }
         }
         System.out.println(bikeList.toString());
-        System.out.println("생성된 1인용 자전거 대수: "+ countSingle);
-        System.out.println("생성된 2인용 자전거 대수: "+ countTwin);
+        System.out.println("생성된 1인용 자전거 대수: " + countSingle);
+        System.out.println("생성된 2인용 자전거 대수: " + countTwin);
         return bikeList;
     }
 
     void removeBike() {
 
 
+    }
+
+    void adminLogin() {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println("관리자 ID를 입력해주세요");
+            String id = sc.nextLine().trim().toLowerCase();
+
+            System.out.println("관리자 비밀번호를 입력해주세요");
+            String pwd = sc.nextLine().trim();
+
+            if (!adminMap.containsKey(id)) {
+                System.out.println("id가 맞지 않습니다. 재입력 해주세요.");
+                return;
+            } else {
+                if (adminMap.get(id).equals(pwd)) {
+                    System.out.println("관리자 인증이 완료되었습니다.");
+                    menu.displayAdminMenu();
+                    break;
+                } else {
+                    System.out.println("비밀번호가 맞지 않습니다. 재입력 해주세요");
+                    break;
+                }
+            }
+        }
     }
 }
--- PJ01_001/src/kr/or/tashow/Menu.java
+++ PJ01_001/src/kr/or/tashow/Menu.java
@@ -21,56 +21,63 @@ public class Menu {
         io.startSystem();
     }
 
-
     public void displayDefaultMenu() {
         // 회원가입, 사용자 인증, 관리자 인증, 프로그램 종료
-        while(true) {
-            System.out.println("원하는 메뉴를 입력하세요");
+        while (true) {
+            System.out.println("원하는 메뉴의 번호를 입력하세요");
             System.out.println("1. 회원가입");
             System.out.println("2. 사용자인증");
             System.out.println("3. 관리자인증");
             System.out.println("4. 프로그램종료");
             int menu = Integer.parseInt(input.nextLine());
 
-            if(menu >= 1 && menu <= 4) {
-                switch (menu) {
-                    case 1:
-                        userSystem.singUp();
-                        break;
-                    case 2:
-                        userSystem.userLogin();
-                        displayUserMenu();
-                        break;
-                    case 3: displayAdminMenu(); break;
-                    case 4: return;
-                }
-            } else {
-                System.out.println("잘못입력");
-                return;
+            switch (menu) {
+                case 1:
+                    userSystem.singUp();
+                    break;
+                case 2:
+                    userSystem.userLogin();
+                    displayUserMenu();
+                    break;
+                case 3:
+                    adminSystem.adminLogin();
+                    break;
+                case 4:
+                    return;
+                default:
+                    System.out.println("잘못입력");
+                    continue;
             }
+
         }
     }
 
     void displayAdminMenu() {
         // 매출관리, 회원목록조회, 자전거관리, 프로그램 종료
-        while(true) {
-            System.out.println("원하는 메뉴를 입력하세요");
+        while (true) {
+            System.out.println("원하는 메뉴의 번호를 입력하세요");
             System.out.println("1. 매출조회");
             System.out.println("2. 회원목록조회");
             System.out.println("3. 자전거관리");
             System.out.println("4. 이전메뉴");
 
             int menu = Integer.parseInt(input.nextLine());
-            if(menu >= 1 && menu <= 4) {
-                switch (menu) {
-                    case 1: bikeService.calculateTotalSales(); break;
-                    case 2: io.readUserList(); break;
-                    case 3: displayBikeMenu(); break;
-                    case 4: return;
-                }
-            } else {
-                System.out.println("잘못입력");
-                return;
+
+            switch (menu) {
+                case 1:
+                    bikeService.calculateTotalSales();
+                    break;
+                case 2:
+                    io.readUserList();
+                    break;
+                case 3:
+                    displayBikeMenu();
+                    break;
+                case 4:
+                    return;
+                default:
+                    System.out.println("잘못입력");
+                    continue;
             }
         }
     }
@@ -78,36 +85,40 @@ public class Menu {
     void displayUserMenu() {
         // 대여하기 (> 자전거 목록조회), 반납하기 (> 결제 및 반납), 프로그램 종료
 
-        while(true) {
-
+        while (true) {
+            System.out.println("원하는 메뉴의 번호를 입력하세요");
             System.out.println("1. 대여하기 (1인용)");
             System.out.println("2. 대여하기 (2인용)");
             System.out.println("3. 결제 및 반납하기");
             System.out.println("4. 이전메뉴");
 
             int menu = Integer.parseInt(input.nextLine());
-            if(menu >= 1 && menu <= 4) {
-                switch (menu) {
-                    case 1: bikeService.rentalBike("S"); break;
-                    case 2: bikeService.rentalBike("T"); break;
-                    case 3:
-                        System.out.println("반납할 자전거의 일련번호를 입력해주세요");
-                        String id = input.nextLine();
-                        bikeService.returnBike(id);
-                        break;
-                    case 4: return;
-                }
-            } else {
-                System.out.println("잘못입력");
-                return;
+
+            switch (menu) {
+                case 1:
+                    bikeService.rentalBike("S");
+                    break;
+                case 2:
+                    bikeService.rentalBike("T");
+                    break;
+                case 3:
+                    System.out.println("반납할 자전거의 일련번호를 입력해주세요");
+                    String id = input.nextLine();
+                    bikeService.returnBike(id);
+                    break;
+                case 4:
+                    return;
+                default:
+                    System.out.println("잘못입력");
+                    continue;
             }
         }
     }
 
     void displayBikeMenu() {
 
-        while(true) {
-
+        while (true) {
+            System.out.println("원하는 메뉴의 번호를 입력하세요");
             System.out.println("1. 자전거등록");
             System.out.println("2. 자전거삭제");
             System.out.println("3. 자전거목록조회");
@@ -115,20 +126,26 @@ public class Menu {
             System.out.println("5. 이전메뉴");
 
             int menu = Integer.parseInt(input.nextLine());
-            if(menu >= 1 && menu <= 4) {
-                switch (menu) {
-                    case 1:
-                        adminSystem.addBike();
-                        io.writeBikeList();
-                        break;
-                    case 2: adminSystem.removeBike(); break;
-                    case 3: io.readBikeList(); break;
-                    case 4: io.readRentList(); break;
-                    case 5: return;
-                }
-            } else {
-                System.out.println("잘못입력");
-                return;
+
+            switch (menu) {
+                case 1:
+                    adminSystem.addBike();
+                    io.writeBikeList();
+                    break;
+                case 2:
+                    adminSystem.removeBike();
+                    break;
+                case 3:
+                    io.readBikeList();
+                    break;
+                case 4:
+                    io.readRentList();
+                    break;
+                case 5:
+                    return;
+                default:
+                    System.out.println("잘못입력");
+                    continue;
             }
         }
     }
